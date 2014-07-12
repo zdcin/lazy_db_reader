27e04ce5703b6c227208f582dcd473d5c193c276
Zhang Dong <dong.zhang@secneo.com>
2014-07-12 13:49:06 +0800

    重构sample2，把配置信息汇总

--- src/lazy_db_reader/sample.clj
+++ src/lazy_db_reader/sample.clj
@@ -6,19 +6,23 @@
            [org.apache.hadoop.hbase.client HTable Put])
   (:gen-class))
 
+(def conf {
+  :db-conf {:subprotocol "mysql"
+           :subname     ""
+           :user        ""
+           :password    ""}
+  :fetch-size 2000
+  :query-init-value {:mf_md5 ""}
+  :write-batch-size 1000
+  :htable-name "install_summary_mf"
+})
 
 ;(require '[clojure.java.jdbc :as j])
 
 (defn my-load-fn [current_value]
-  (let [mysql-db {:subprotocol "mysql"
-                  :subname     ""
-                  :user        ""
-                  :password    ""}]
-    (j/query mysql-db
-             ["select pkg_name, mf_md5, cert_md5 from installed_apks where mf_md5>? order by mf_md5 asc limit 2000"
-              (:mf_md5 current_value)])))
-
-
+  (j/query (:db-conf conf)
+           ["select pkg_name, mf_md5, cert_md5 from installed_apks where mf_md5>? order by mf_md5 asc limit ?"
+            (:mf_md5 current_value) (:fetch-size conf)]))
 
 (defn to-put [item]
   (let [;_ (println item)
@@ -31,16 +35,15 @@
 
 
 (defn -main []
-  (let [
+  (let [ batch-size (:write-batch-size conf)
          _ (println "in main...")
          my-table-seq (table-query-seq (->QueryState (ref {:mf_md5 ""}) (ref []) my-load-fn))
          ;__ (println (first my-table-seq))
-         table (new HTable (HBaseConfiguration/create) "install_summary_mf")] ;install_summary_mf
+         table (new HTable (HBaseConfiguration/create) (:htable-name conf))] ;install_summary_mf
     (dorun
       (map (fn [item-list]
-             (let [puts
-                   (map to-put item-list)]
+             (let [puts (map to-put item-list)]
                (. table put puts)))
-           (partition 1000 1000 nil my-table-seq))          ;end map
+           (partition batch-size batch-size nil my-table-seq))          ;end map
                                                    ;			(. table close)
       )))
